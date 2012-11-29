1afbb9e1ee20466723e6ddf717aa6a220cf5e82a
Mike-Smith <zhou.yun.x@gmail.com>
2012-11-30 00:53:08 +0200

    arrange the folder

--- include/NB.h
+++ include/NB.h
@@ -7,10 +7,18 @@
  * layer.
  * Above math layer is physics level, which reduces physical concepts into mathematical concepts.
  * Above physics layer is methods level which defines methods on physical systems
- * In the math level there should be only several objects, numbers, vectors, matrix. it should have
+ * In the math level there should be only several classes, numbers, vectors, matrix. it should have
  * methods linear algebra, differential equation, optimization, ...
  * In the physics layer, there should be objects time, space, matter, change.
- * In the method layer, there should be objects concept,statistics, experiments, feedback, reactions
+ * In the method layer, there should be objects experiments, feedback, reactions
  * and so on.
+ * There should be reduction method for every concept beyond math layer. Every object should be reduced
+ * to objects in the lower layer.
  */
+namespace NB{
+#include "nb_math.h"
+#include "nb_physics.h"
+#include "nb_method.h"
+}
+
 #endif
