c08f9713450bdff2809a5057069fa3e1d3c299e4
Mike-Smith <zhou.yun.x@gmail.com>
2012-11-30 23:25:17 +0200

    first compile static lib with executable works well

---
+++ test/src/test.cpp
@@ -0,0 +1,19 @@
+/*
+ * test.cpp
+ *
+ *  Created on: Nov 30, 2012
+ *      Author: yun
+ */
+
+
+
+
+#include "NB.h"
+
+using namespace NB;
+int main()
+{
+	NB::Matrix a;
+	a.create(2,2);
+	return 0;
+}
